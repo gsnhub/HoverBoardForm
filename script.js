024d96314ff954ebd0a3a3e56154415075e76531
gsn <swaminathan.g007@gmail.com>
2022-12-20 12:56:40 +0530

    hoverboard animated

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const container = document.querySelector('.container');
 const colors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71']
-const totalSquares = 500;
+const totalSquares = 15000;
 
 for(let i=1;i<totalSquares;i++){
     let square = document.createElement('div');
@@ -19,6 +19,6 @@ let setColor = (item) =>  {
     item.style.boxShadow = `0 0 2px ${color},0 0 10px ${color}`;
 }
 let removeColor = (item) =>{
-    item.style.backgroundColor = `#333`;
+    item.style.backgroundColor = `#1d1d1d`;
     item.style.boxShadow = `0 0 2px #000`
 }
