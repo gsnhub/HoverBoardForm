c22d27c7bdb733b44211d48d4b79b050d118d7f9
gsn <swaminathan.g007@gmail.com>
2022-12-20 12:08:57 +0530

    hoverboard created

---
+++ script.js
@@ -0,0 +1,24 @@
+const container = document.querySelector('.container');
+const colors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71']
+const totalSquares = 500;
+
+for(let i=1;i<totalSquares;i++){
+    let square = document.createElement('div');
+    square.classList.add('squareCSS');
+    square.addEventListener('mouseover',() => setColor(square));
+    square.addEventListener('mouseout',() => removeColor(square));
+    container.appendChild(square);
+}
+
+let getRandomColor = () =>{
+    return colors[Math.floor(Math.random()*colors.length)];
+}
+let setColor = (item) =>  {
+    let color = getRandomColor();
+    item.style.backgroundColor = color;
+    item.style.boxShadow = `0 0 2px ${color},0 0 10px ${color}`;
+}
+let removeColor = (item) =>{
+    item.style.backgroundColor = `#333`;
+    item.style.boxShadow = `0 0 2px #000`
+}
